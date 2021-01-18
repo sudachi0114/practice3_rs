baa2abb1ec2502aa26a43b7e170e089f6de1b281
sudachi0114 <jnevariste@gmail.com>
2021-01-19 02:02:06 +0900

    摂氏から華氏に変換する関数を作った

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    convert_c2f(10);
+}
+
+// 摂氏 -> 華氏を変換する関数
+fn convert_c2f(n: i32) -> () {
+    let answer = (n * 9 / 5) + 32;
+    println!("answer: {}", answer);
 }
