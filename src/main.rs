d3c3d5b47a38e56885ef5e2cdc7f17fd08e59806
sudachi0114 <jnevariste@gmail.com>
2021-01-20 03:03:30 +0900

    fibonacci数列の第n項目を返す関数を定義

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,23 @@
 fn main() {
-    convert_c2f(25);
-    convert_f2c(77);
+    let n = 7;
+    let answer = fibonacci(n);
+    println!("The {}th item of fibonacci-sq is {}", n, answer);
 
-    convert_cf(25, 'c');
-    convert_cf(77, 'f');
-    convert_cf(9999, 'z');
-}
+    let mut idx = 1;
+    while idx < 15 {
+        let answer = fibonacci(idx);
+        println!("The {}th item of fibonacci-sq is {}", idx, answer);
 
-// 摂氏 -> 華氏を変換する関数
-fn convert_c2f(n: i32) -> () {
-    let answer = (n * 9 / 5) + 32;
-    println!("answer: {}", answer);
-}
-
-// 摂氏 <- 華氏を変換する関数
-fn convert_f2c(n: i32) -> () {
-    let answer = (n - 32) * 5 / 9;
-    println!("answer: {}", answer);
-}
-
-// 摂氏 <-> 華氏を変換する関数
-//   n   : 数値 (摂氏や華氏で表された温度の数値)
-//   unit: 単位 (摂氏: c / 華氏: f)
-fn convert_cf(n: i32, unit: char) -> () {
-    let answer = if unit == 'c' {  // 摂氏 -> 華氏
-        (n * 9 / 5) + 32
-    } else if unit == 'f' {  // 華氏 -> 摂氏
-        (n - 32) * 5 / 9
-    } else {  // unit が 'c' でも 'f' でもない場合
-        -1  // raise Error
-    };
-
-    let answer_unit = if unit == 'c' {
-        'f'
-    } else if unit == 'f' {
-        'c'
-    } else {
-        'x' // raise Error
-    };
-
-    if answer != -1 || answer_unit != 'x' {
-        println!("answer: {} {}", answer, answer_unit);
-    } else {
-        println!("[Error] something wrong... @input_parameter: ({}, {})", n, unit);
+        idx = idx + 1;
     }
+}
+
+// フィボナッチ数列の第n項目を求めるプログラム
+fn fibonacci(n: u32) -> u32 {
+    let answer = if n == 1 || n == 2 {
+        1
+    } else {
+        fibonacci(n-1) + fibonacci(n-2)
+    };
+    answer
 }
