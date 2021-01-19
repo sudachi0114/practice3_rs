91820b6ea28f753c3fa8d5c38ceb82a5991c8afc
sudachi0114 <jnevariste@gmail.com>
2021-01-20 03:14:43 +0900

    for loop をつかって第n項を求める別解も思いついたので作ってみた

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ fn main() {
     let mut idx = 1;
     while idx < 15 {
         let answer = fibonacci(idx);
-        println!("The {}th item of fibonacci-sq is {}", idx, answer);
+        println!("[check] The {}th item of fibonacci-sq is {}", idx, answer);
 
         idx = idx + 1;
     }
+
+    let fl_ans = fibonacci_loop(n);
+    println!("[fibonacci-loop] answer: {}", fl_ans);
 }
 
 // フィボナッチ数列の第n項目を求めるプログラム
@@ -18,5 +21,20 @@ fn fibonacci(n: u32) -> u32 {
         1
     } else {
         fibonacci(n-1) + fibonacci(n-2)
+    }
+}
+
+fn fibonacci_loop(n: u32) -> u32 {
+    let mut a = 1;
+    let mut b = 1;
+    if n > 2 {
+        for _ in 2..n {
+            let tmp = b;
+            b = b + a;
+            a = tmp;
+        }
+        b  // return
+    } else {
+        1
     }
 }
